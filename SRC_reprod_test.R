8489e2526d8b591c00ae28530300092bb7f6ef22
Petr Vesnovskii <petrvesnovskii@eduroam-10-200-34-232.local.su.se>
2024-11-22 13:06:39 +0100

    test 2

---
+++ SRC_reprod_test.R
@@ -0,0 +1,7 @@
+install.packages("ncdf4", "raster", "rgdal", "ggplot")
+library(ncdf4) # package for netcdf manipulation
+library(raster) # package for raster manipulation
+library(rgdal) # package for geospatial analysis
+library(ggplot2) # package for plotting
+
+# efed 
